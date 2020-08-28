182f047c513663a48a118bc09caed2765beeb977
Ivanilson Sousa <ivanilsonsousa95@gmail.com>
2020-08-27 21:54:36 -0300

    refactory

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const routes = require('./routes');
 const app = express();
